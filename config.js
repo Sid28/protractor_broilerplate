9084653f45dcbe0524f2641f183fc795bb31eee9
sidmishra <sidmishra@CORP.AMAZONWORKSPACES.COM>
2020-09-01 18:06:10 +0530

    sadsada

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.config = {
   chromeDriver: './chromedriver',
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
-  // seleniumServerJar: 'selenium-server-3.141.59.jar',
-  directConnet:true,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumServerJar: 'selenium-server-3.141.59.jar',
+  // directConnet:true,
   specs: ['spec.js'],
   capabilities : {
     browserName: 'chrome',

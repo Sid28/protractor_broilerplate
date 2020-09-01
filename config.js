02555d4c0e99f266e99b4c231ba5c26262302b67
sidmishra <sidmishra@CORP.AMAZONWORKSPACES.COM>
2020-09-01 17:49:46 +0530

    Added changes

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.config = {
+  chromeDriver: 'chromedriver',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumServerJar: 'selenium-server-3.141.59.jar',
+  directConnet:true,
+  specs: ['spec.js']
+};

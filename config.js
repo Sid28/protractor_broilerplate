2b82b6e72a280e365dac06500bd3bd61dc21c392
sidmishra <sidmishra@CORP.AMAZONWORKSPACES.COM>
2020-09-01 17:56:05 +0530

    sjdhsakjdas

--- config.js
+++ config.js
@@ -3,5 +3,13 @@ exports.config = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   // seleniumServerJar: 'selenium-server-3.141.59.jar',
   directConnet:true,
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  capabilities = {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 1,
+    'chromeOptions': {
+        'args': ['start-minimized', 'window-size=1920,1080']
+    }
+}
 };

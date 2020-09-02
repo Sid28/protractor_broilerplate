cef0734fda4133e8a4becdb2bcc92611517f5fba
Sid28 <mishra.siddharth90@gmail.com>
2020-09-02 17:01:08 +0530

    Update config.js
    asdad

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var jar = require('selenium-server-standalone-jar');
 
 exports.config = {
   chromeDriver: './chromedriver',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+//   seleniumAddress: 'http://localhost:4444/wd/hub',
   seleniumServerJar: jar.path,
   // directConnet:true,
   specs: ['spec.js'],

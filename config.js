0c07d865e5ec45b88f484aae015290cb1c92e797
Sid28 <mishra.siddharth90@gmail.com>
2020-09-14 13:04:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ exports.config = {
 //   seleniumServerJar: './selenium-server-3.141.59.jar',
   directConnet:true,
   specs: ['spec.js'],
-//   capabilities : {
-//     browserName: 'chrome',
-//     shardTestFiles: true,
-//     maxInstances: 1,
-//     'chromeOptions': {
-//         'args': ['start-minimized', 'window-size=1920,1080','--headless']
-//     }
-// }
+  capabilities : {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 1,
+    'chromeOptions': {
+        'args': ['start-minimized', 'window-size=1920,1080','--headless']
+    }
+}
 };

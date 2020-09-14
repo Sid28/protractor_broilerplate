bfab00e437a14a714232f6d3957ab93dc8e5d130
Sid28 <mishra.siddharth90@gmail.com>
2020-09-14 12:45:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 var jar = require('selenium-server-standalone-jar');
 
 exports.config = {
-  //chromeDriver: './chromedriver',
+  chromeDriver: './chromedriver',
 //   seleniumAddress: 'http://localhost:4444/wd/hub',
 //   seleniumServerJar: './selenium-server-3.141.59.jar',
-  //directConnet:true,
+  directConnet:true,
   specs: ['spec.js'],
-  capabilities : {
-    browserName: 'chrome',
-    shardTestFiles: true,
-    maxInstances: 1,
-    'chromeOptions': {
-        'args': ['start-minimized', 'window-size=1920,1080','--headless']
-    }
+//   capabilities : {
+//     browserName: 'chrome',
+//     shardTestFiles: true,
+//     maxInstances: 1,
+//     'chromeOptions': {
+//         'args': ['start-minimized', 'window-size=1920,1080','--headless']
+//     }
 }
 };
